521ed420148a36184f39a25d4780c9c1fc852dec
Byurhan Beyzat <imbyurhan@gmail.com>
2019-05-06 17:16:45 +0300

    Initial commit 🚀

---
+++ gatsby-node.js
@@ -0,0 +1,112 @@
+const Gitlab = require('gitlab/dist/es5').default;
+const crypto = require('crypto');
+
+exports.sourceNodes = async ({ actions: { createNode } }, configOptions) => {
+  if (!configOptions.accessToken) {
+    throw 'You need to enter an accessToken';
+  }
+
+  const api = new Gitlab({
+    token: configOptions.accessToken,
+  });
+
+  const user = await api.Users.current();
+  const projects = await api.Projects.all({ owned: true, visibility: 'public' });
+
+  projects.map(async project => {
+    const jsonString = JSON.stringify(project);
+
+    const projectNode = {
+      projectID: project.id,
+      id: project.id.toString(),
+      description: project.description,
+      visibility: project.visibility,
+      default_branch: project.default_branch,
+      ssh_url_to_repo: project.ssh_url,
+      http_url_to_repo: project.http_url,
+      web_url: project.web_url,
+      tag_list: project.tag_list,
+      readme_url: project.readme_url,
+      avatar_url: project.avatar_url,
+      owner: {
+        id: project.owner.id.toString(),
+        name: project.owner.name,
+        created_at: project.owner.created_at,
+      },
+      name: project.name,
+      name_with_namespace: project.name_with_namespace,
+      path_with_namespace: project.path_with_namespace,
+      path: project.path,
+      issues_enabled: projects.issues_enabled,
+      open_issues_count: projects.open_issues_count,
+      merge_requests_enabled: projects.merge_requests_enabled,
+      jobs_enabled: projects.jobs_enabled,
+      wiki_enabled: projects.wiki_enabled,
+      created_at: project.created_at,
+      last_activity_at: project.last_activity_at,
+      creator_id: project.creator_id.toString(),
+      namespace: {
+        id: project.namespace.id.toString(),
+        name: project.namespace.name,
+        path: project.namespace.path,
+        kind: project.namespace.kind,
+        full_path: project.namespace.full_path,
+      },
+      import_status: project.import_status,
+      archived: project.archived,
+      forks_count: project.forks_count.toString(),
+      star_count: project.star_count.toString(),
+      _links: {
+        self: project._links.self,
+        issues: project._links.issues,
+        merge_requests: project._links.merge_requests,
+        repo_branches: project._links.repo_branches,
+        labels: project._links.labels,
+        events: project._links.events,
+        members: project._links.members,
+      },
+      children: [],
+      parent: `__SOURCE__`,
+      internal: {
+        type: `GitlabProjects`,
+        contentDigest: crypto
+          .createHash(`md5`)
+          .update(jsonString)
+          .digest(`hex`),
+      },
+    };
+
+    createNode(projectNode);
+  });
+
+  const jsonStringUser = JSON.stringify(user);
+
+  const userNode = {
+    userID: user.id,
+    bio: user.bio,
+    name: user.name,
+    email: user.email,
+    state: user.state,
+    username: user.username,
+    profile_url: user.web_url,
+    location: user.location,
+    avatar: user.avatar_url,
+    website: user.website_url,
+    organization: user.organization,
+    socialMedia: {
+      skype: user.skype,
+      linkedin: user.linkedin,
+      twitter: user.twitter,
+    },
+    id: user.id.toString(),
+    parent: `__SOURCE__`,
+    internal: {
+      type: `GitlabUser`,
+      contentDigest: crypto
+        .createHash(`md5`)
+        .update(jsonStringUser)
+        .digest(`hex`),
+    },
+  };
+  createNode(userNode);
+};
---
+++ readme.md
@@ -0,0 +1,116 @@
+# gatsby-source-gitlab
+> Gatsby.js source plugin for loading information from GitLab
+
+Learn more about [Gatsby](https://www.gatsbyjs.org/) and its plugins here: [https://www.gatsbyjs.org/docs/plugins/](https://www.gatsbyjs.org/docs/plugins/) <br />
+*Live example site coming soon!*
+
+## Install
+
+```bash
+npm install gatsby-source-gitlab
+# OR
+yarn add gatsby-source-gitlab
+```
+
+## How to use
+
+```js
+// gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-source-gitlab`,
+    options: {
+      // You can get your access token on your GitLab profile
+      accessToken: 'your access token here',
+    }
+  },
+]
+```
+
+## GraphQL Queries
+
+To see all possible queries please use the GraphiQL editor which is available under ``http://localhost:8000/___graphql``
+
+### Get all projects of the user:
+
+```graphql
+query {
+  allGitlabRepositories {
+    edges {
+      node {
+        id
+        name
+        description
+        created_at
+        visibility
+        default_branch
+        web_url
+        readme_url
+        owner {
+          id
+          name
+        }
+        forks_count
+        star_count
+        archived
+        import_status
+        name_with_namespace
+        path_with_namespace
+        path
+        last_activity_at
+        creator_id
+        namespace {
+          id
+          name
+          path
+          kind
+          full_path
+        }
+        _links {
+          self
+          issues
+          merge_requests
+          repo_branches
+          labels
+          events
+          members
+        }
+      }
+    }
+  }
+}
+```
+
+
+### Get all user information:
+
+```graphql
+query {
+  allGitlabUser {
+    edges {
+      node {
+        id
+        name
+        email
+        username
+        bio
+        state
+        avatar
+        location
+        website
+        profile_url
+        organization
+        socialMedia {
+          skype
+          twitter
+          linkedin
+        }
+      }
+    }
+  }
+}
+```
+
+## License
+[MIT](./license) &copy; [Byurhan Beyzat](https://byurhanbeyzat.com/). <br />
+[Buy me a coffee](https://ko-fi.com/X7X38NNC)
